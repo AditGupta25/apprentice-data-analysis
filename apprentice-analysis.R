cd3c7756c0bbae67893634ca8f46867a7267283e
Adit Gupta <aditgupta1@yahoo.com>
2024-09-22 08:40:48 -0400

    initial upload

---
+++ apprentice-analysis.R
@@ -0,0 +1,10 @@
+library(readr)
+
+demographics <- read_csv("Documents/GitHub/apprentice-data-analysis/demographics.csv")
+View(demographics)
+
+grades <- read_csv("Documents/GitHub/apprentice-data-analysis/grades.csv")
+View(grades)
+
+transactions <- read_csv("Documents/GitHub/apprentice-data-analysis/transactions.csv")
+View(transactions)
